3cb8cd557c9d1eeaf6fa018d809a285691503ea2
hQian2718 <22kqian@athenian.org>
2020-11-07 10:33:52 +0000

    implemented three methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,26 +2,47 @@ import java.util.ArrayList;
 
 public class MyMain {
 
-    // Counts the number of odd numbers in list
-    public static int countOdd(ArrayList<Integer> list) { 
-        // REPLACE WITH YOUR CODE
-        return 0;
-    }
-
-    // Returns true if there is an int that appears in both lists
-    public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2) { 
-        // REPLACE WITH YOUR CODE
-        return false;
-    }
-
-    // Takes an int[] as input and returns the equivalent ArrayList<Integer>
-    public static ArrayList<Integer> convertToArrayList(int[] arr) { 
-        // REPLACE WITH YOUR CODE
-        return null;
-    }
-
+    	public static int countOdd(ArrayList<Integer> list){
+		int c = 0;
+		for (int i = 0; i < list.size(); i ++){
+			if(list.get(i) % 2 != 0){
+				c++;
+			}
+		}
+		return c;
+	}
+	
+	public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2){
+		for(int i = 0; i < list1.size();i++){
+			for(int j = 0; j < list2.size(); j++){
+				if(list1.get(i) == list2.get(j)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public static ArrayList<Integer> convertToArrayList(int[] arr){
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		for(int i = 0; i < arr.length; i ++){
+			list.add(arr[i]);
+		}
+		
+		return list;		
+	}
+	
 
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        int[] arr1 = {2,3,4,5,6,7};
+		ArrayList<Integer> list = convertToArrayList(arr1);
+		
+		int[] arr2 = {9,8,7,6,5};
+		ArrayList<Integer> list1 = convertToArrayList(arr2);
+		
+		System.out.println(list);
+		System.out.println(checkDuplicates(list,list1));
+		System.out.println(countOdd(list));
     }
 }
